354d4659d92689f031b8b761554777527a755942
gj-ca <56330692+gj-ca@users.noreply.github.com>
2019-10-26 08:58:28 +1000

    Update to rb file

---
+++ github.rb
@@ -0,0 +1,18 @@
+number_of_commits = 0
+commit = false
+changes = false
+
+puts "There are #{changes == true ? "" : "no"} changes".
+
+# Make a change here.
+changes = false
+
+if changes == true
+  puts "we need to add and commit our changes"
+  commit = true
+  commit += 1
+end
+
+if commit = true
+  puts "We push our changes to github"
+end
